ce7f836efff91ef13dac0e23155a8a4745701146
abmantis <amfcalt@gmail.com>
2020-11-09 23:29:56 +0000

    Add whirlpool component

---
+++ whirlpool/__init__.py
@@ -0,0 +1,36 @@
+"""Whirlpool's Sixth Sense integration."""
+
+from whirlpool.auth import Auth
+import logging
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "whirlpool"
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_USERNAME): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def async_setup(hass, config):
+    username = config[DOMAIN][CONF_USERNAME]
+    password = config[DOMAIN][CONF_PASSWORD]
+    auth = Auth(username, password)
+
+    hass.data[DOMAIN] = {"auth": auth}
+
+    hass.helpers.discovery.load_platform("climate", DOMAIN, {}, config)
+
+    return True
